5a9653477ae596a681317c01d78595bf9c0f9353
Annie Cai <caishajia@gmail.com>
2023-02-16 10:00:12 -0500

    created new table to store products

---
+++ backend (spring)/src/main/java/com/cafe/com/cafe/modal/Product.java
@@ -0,0 +1,41 @@
+package com.cafe.com.cafe.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "product")
+public class Product implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    // many to one --> one row in a table is mapped to multiple rows in another table
+    @ManyToOne(fetch = FetchType.LAZY) // lazy = fetch data when needed
+    @JoinColumn(name = "category_fk", nullable = false)
+    private Category category;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "status")
+    private String status;
+
+
+}
