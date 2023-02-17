d05fcd1dad32e86842866d004bd5a822bde39910
Annie Cai <caishajia@gmail.com>
2023-02-16 21:31:21 -0500

    finished all apis! implemented an api to return count of each database

--- backend (spring)/src/main/java/com/cafe/com/cafe/service/CategoryService.java
+++ backend (spring)/src/main/java/com/cafe/com/cafe/service/CategoryService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 public interface CategoryService {
-
     // adds new category to the category api
     ResponseEntity<String> addNewCategory(Map<String, String> requestMap);
 
