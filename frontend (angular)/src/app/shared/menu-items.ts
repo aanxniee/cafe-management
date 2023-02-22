46ba344043624371896cda616aaf287b9c6711fd
Annie Cai <caishajia@gmail.com>
2023-02-22 10:22:29 -0500

    fixed dashboard and sidebar ui + added category post/get requests

--- frontend (angular)/src/app/shared/menu-items.ts
+++ frontend (angular)/src/app/shared/menu-items.ts
@@ -4,12 +4,12 @@ export interface Menu {
     state:string;
     name:string;
     type:string;
-    icon:string;
     role:string;
 }
 
 const MENUITEMS = [
-    {state:'dashboard', name:'Dashboard', type:'link', icon:'dashboard', role:''}
+    {state:'dashboard', name:'Dashboard', type:'link', role:''},
+    {state:'catergory', name:'Manage Categories', type:'link', role:'admin'}
 ]
 
 @Injectable()
